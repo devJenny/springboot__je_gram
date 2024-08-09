1d01e851c9b0d26bf2aade23123e0ff1288d4180
devjenny <devje@naver.com>
2024-08-09 18:55:24 +0900

    Build: created gradle & yml file

---
+++ src/main/java/org/example/photogram_re/PhotogramReApplication.java
@@ -0,0 +1,13 @@
+package org.example.photogram_re;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotogramReApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotogramReApplication.class, args);
+    }
+
+}
