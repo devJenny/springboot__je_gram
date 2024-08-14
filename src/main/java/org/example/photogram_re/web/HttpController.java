185836e3e851aeff713c8a61a6b8d4c1f7efcf05
devjenny <devje@naver.com>
2024-08-14 18:28:22 +0900

    Fix: Updated Jsp to Thymeleaf

---
+++ src/main/java/org/example/photogram_re/web/HttpController.java
@@ -0,0 +1,22 @@
+package org.example.photogram_re.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HttpController {
+//    public String get() {
+//
+//    }
+//
+//    public String post() {
+//
+//    }
+//
+//    public String put() {
+//
+//    }
+//
+//    public String delete() {
+//
+//    }
+}
