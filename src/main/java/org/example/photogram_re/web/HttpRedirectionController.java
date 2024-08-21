bf4f0ef7f58837417406b2ba0b7592536d665b9d
devjenny <devje@naver.com>
2024-08-21 18:47:47 +0900

    Test: Test about `Redirection`

---
+++ src/main/java/org/example/photogram_re/web/HttpRedirectionController.java
@@ -0,0 +1,21 @@
+package org.example.photogram_re.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HttpRedirectionController {
+
+    @GetMapping("/home")
+    public String home() {
+        // 1만줄 가정
+        return "home";
+
+    }
+
+    @GetMapping("/away")
+    public String away() {
+        // 다른 코드
+        return "redirect:/home";
+    }
+}
